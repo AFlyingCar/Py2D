894298f25ab7cbe2d997a109827d9121f2aa1f78
Tyler Robbins <tyler@familyrobbins.com>
2015-03-03 09:37:53 -0800

    Added method to return true if a blank line is encountered in MANIFEST. Added lines to strip whitespace from the beginnings and ends of a line in MANIFEST. Fixed formatting on informational output.

--- Py2D_installer.py
+++ Py2D_installer.py
@@ -30,6 +30,16 @@ def getFile(url,install_path):
 		print "Error downloading file: %s\n" % url.split(MASTER_DOWNLOAD_URL)[1] 
 		return
 
+def isBlank(string):
+	string = string.strip()
+	string = string.rstrip()
+	string = string.replace(" ","")
+	string = string.replace("\t","")
+	string = string.replace("\n","")
+	string = string.replace("\r","")
+
+	return len(string) < 1
+
 #
 # Write a string as executable Python code to path
 # Code mostly copied from Python's compile module
@@ -56,6 +66,9 @@ def writeStringToPYC(data,path):
 	fc.close()
 
 def PrepareManifestVal(man_value):
+	man_value = man_value.rstrip() # Remove all /r values
+	man_value = man_value.strip()
+
 	url = MASTER_DOWNLOAD_URL + man_value[1:]
 
 	directory = man_value[2:]
@@ -83,10 +96,17 @@ def main():
 	for f in flist:
 		url,path,filepath = PrepareManifestVal(f)
 
+		if isBlank(f):
+			continue
+
 		if not os.path.exists(path):
 			os.mkdir(path)
 		
-		print "Downloading %s...\n"%f[2:]
+		#
+		# Format output to prevent large amounts of console spam
+		# Output should look like this: Downloading C:\Program Files\...\path\to\file
+		#
+		print "Downloading %s\n" %(path[:17] + "..." + filepath[len(filepath)-len(f[2:]):]) 
 
 		getFile(url,filepath + "c")
 
