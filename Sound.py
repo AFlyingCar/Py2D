c56f51e4fd1e5a47c7b5f052de482134840a02dd
Tyler Robbins <tyler@familyrobbins.com>
2015-04-01 11:22:39 -0700

    Added class and method descriptions. Added forward declaration for sound. Added isFinished and setMaxChannelNums methods.

--- Sound.py
+++ Sound.py
@@ -2,8 +2,18 @@ import pygame.mixer, os
 
 class Sound(object):
 	def __init__(self,max_channels,file_obj=None,filename="",channel_num=0):
+		'''Improved sound object that stores much more information.
+		max_channels = Maximum amount of allowed channels.
+		file_obj	 = File object to load sound from.
+		filename 	 = Name of a file to load a sound from.
+		channel_num	 = Channel number.
+
+		channel 	 = Channel to use.'''
+
 		if 0 > channel_num > max_channels:
 			raise ValueError("channel_num must be greater than 0 and less than %d"%max_channels)
+
+		self.sound = None #Forward declaration
 
 		if filename:
 			self.loadFile(filename)
@@ -18,13 +28,22 @@ class Sound(object):
 		self.filename = filename
 
 	def play(self):
+		'''Play the current loaded sound and return True if successful. If no sound is loaded, return False.'''
+
 		if self.sound:
 			self.channel.play(self.sound)
 			return True
 		
 		return False
 
+	def isFinished(self):
+		'''Return True if the sound has finished playing. False otherwise.'''
+		
+		return not self.channel.get_busy()
+
 	def stop(self):
+		'''Stop playing the sound on the current channel.'''
+
 		if self.channel.get_busy():
 			self.channel.stop()
 			return True
@@ -32,6 +51,8 @@ class Sound(object):
 		return False
 
 	def loadFile(self,filename):
+		'''Load a sound from a file.'''
+
 		if os.path.exists(filename) or isinstance(filename,file):
 			self.sound = pygame.mixer.Sound(filename)
 			return True
@@ -39,10 +60,14 @@ class Sound(object):
 		return False
 
 	def loadObject(self,file_obj):
+		'''Load a sound object from a file object.'''
+
 		self.sound = pygame.mixer.Sound(file_obj)
 		return True
 
 	def setChannel(self,channel):
+		'''Set the channel to play sound on. Return False if channel is not legal, True otherwise.'''
+
 		if 0 < channel < self.max_channels:
 			return False
 
@@ -53,16 +78,33 @@ class Sound(object):
 		return True
 
 	def getChannelNum(self):
+		'''Return the channel number.'''
+
 		return self.channel_num
 
 	def getChannel(self):
+		'''Return the channel.'''
+
 		return self.channel
 
 	def getFileName(self):
+		'''Return the filename.'''
+
 		return self.filename
 
 	def isPlaying(self):
+		'''Return True if the sound is currently playing, False otherwise.'''
+
 		return self.channel.get_busy()
 
 	def setVolume(self,volume):
+		'''Set the volume of the sound.'''
+
 		self.sound.set_volume(volume)
+
+	def setMaxChannelNums(self,channel):
+		if channel < 0:
+			return False
+
+		self.max_channels = channel
+		return True
