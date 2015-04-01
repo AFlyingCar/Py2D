606d7d7a317620cfdd4b5791c2b6988cb0c634e2
U-Felucia\Tyler <Tyler@Felucia.(none)>
2015-04-01 11:11:57 -0700

    Added SoundManager to import statement. Added soundManager instance. Added soundManager accessor method.

--- IterativeLoop.py
+++ IterativeLoop.py
@@ -1,14 +1,15 @@
 import pygame
 
 # from Managers import *
-from bin.Py2D.Managers import ScreenManager,InputManager
+from bin.Py2D.Managers import ScreenManager,InputManager,SoundManager
 
 # print dir(ScreenManager)
 
 class IterativeLoop(object):
 	def __init__(self,name="Default Name"):
 		self.name = name
-		self.smanager = ScreenManager.ScreenManager.getInstance()
+		self.screenManager = ScreenManager.ScreenManager.getInstance()
+		self.soundManager = SoundManager.SoundManager.getInstance()
 		# self.smanager = ScreenManager.getInstance()
 		# self.smanager.setCameraPosition(500-320,500-240)
 		# self.smanager = ScreenManager.ScreenManager(1000,1000,640,480,campos=[500-320,500-240])
@@ -31,4 +32,7 @@ class IterativeLoop(object):
 		print "Default Interrupted method. Override me!"
 
 	def getScreen(self):
-		return self.smanager
+		return self.screenManager
+
+	def getSound(self):
+		return self.soundManager
