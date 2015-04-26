0ec428ab7e797f7e0dcf9551b538c28f53d18cf9
Tyler Robbins <tyler@familyrobbins.com>
2015-04-26 07:26:10 -0800

    Special animation class that can load sprite sheets.

---
+++ SpriteSheetAnim.py
@@ -0,0 +1,36 @@
+import pygame
+
+import Anim
+
+class SpriteSheetAnim(Anim.Anim):
+	def __init__(self,filename,pos,spriteSize,spriteStartPos=(0,0)):
+		Anim.Anim.__init__(self,[],pos)
+
+		self.filename = filename
+		self.spriteSize = spriteSize
+		self.spriteStartPos = spriteStartPos
+		self.setFrames(self.loadSpriteSheet(filename))
+
+	def getFilename(self):
+		return self.filename
+
+	def loadSpriteSheet(self,filename):
+		image = pygame.image.load(filename)
+		
+		len_sprt_x,len_sprt_y = self.spriteSize
+		sprt_rect_x,sprt_rect_y = self.spriteStartPos
+
+		sheet_rect = image.get_rect()
+		sheet = []
+
+		for row in range(0,sheet_rect.height-len_sprt_y,self.spriteSize[1]):
+			for col in range(0,sheet_rect.width-len_sprt_x,self.spriteSize[0]):
+				image.set_clip(pygame.Rect(sprt_rect_x,sprt_rect_y,len_sprt_x,len_sprt_y))
+				sprite = image.subsurface(image.get_clip())
+				sheet.append(sprite)
+				sprt_rect_x += len_sprt_x
+
+			sprt_rect_y += len_sprt_y
+			sprt_rect_x = 0
+
+		return sheet
