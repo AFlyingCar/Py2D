7569c8fb5d71e8db8c394a064a89bb6d691c308c
Tyler Robbins <tyler@familyrobbins.com>
2015-12-19 20:16:09 -0800

    Added a comment at the top detailing how to run Py2DPackager. Updated help function. Added a new import. Changed how the output file is passed through the command line. Added the option for source files in the specified project to be compiled. Modified the project package loop to check if it should compile the source code before writing it to the package. Improved the code that includes the source code in the package. Added more print statements.

--- Py2DPackager.py
+++ Py2DPackager.py
@@ -1,18 +1,22 @@
 # A simple program that packages Py2D-based projects with Py2D in a zip file.
 # All hidden folders and directories are ignored.
 
+# python Py2DPackager.py [input file] --include-py2d-source --include-project-source --compile-project-source --include-exts=[...] --main=[module] --output=[out.zip]
+
 def help():
     print "Py2DPackager"
-    print "   [input] {output} {options}"
+    print "   [input] {options}"
+    print "   --output={fileName}"
     print "   --include-py2d-source"
     print "   --include-project-source"
     print "   --include-exts=[...]"
     print "   --main={moduleName}"
+    print "   --compile-project-source"
     print ""
     print "   Use the -h flag to display this message and then exit."
     # print "   --compile-project-source"
 
-import sys,zipfile,os
+import sys,zipfile,os,compiler
 
 LOAD_SCRIPT = """
 if __name__ == "__main__":
@@ -35,20 +39,18 @@ if __name__ == "__main__":
     else:
         input_project = sys.argv[1]
 
-    # Create the zip file location
-    if len(sys.argv) >= 3:
-        output_path = sys.argv[2]
-    else:
-        output_path = input_project+".zip"
-
     include_exts=[]
     mainmodule=input_project
+    output_path = input_project+".zip"
     for arg in sys.argv:
         if arg.startswith("--include-exts="):
             include_exts = arg.split("=")[1][1:-1].split(",")
         elif arg.startswith("--main="):
             mainmodule = arg.split("=")[1]
+        elif arg.startswith("--output="):
+            output_path = arg.split("=")[1]
 
+    COMPILE_PROJECT_SOURCE = "--compile-project-source" in sys.argv
     INCLUDE_PROJECT_SOURCE = "--include-project-source" in sys.argv
     INCLUDE_PY2D_SOURCE = "--include-py2d-source" in sys.argv
 
@@ -66,6 +68,7 @@ if __name__ == "__main__":
         print "\t~/.py2d_inst_dir_path is missing."
         sys.exit()
 
+    print "Ready to build " + output_path
     zfile = zipfile.ZipFile(output_path,'w')
 
     # Package execute script
@@ -78,9 +81,26 @@ if __name__ == "__main__":
         dirnames[:] = [d for d in dirnames if not d[0] == '.']
         cpath = os.path.join(os.path.relpath(input_project),os.path.relpath(dirpath))
         for f in filenames:
-            if (INCLUDE_PROJECT_SOURCE and f.endswith(".py")) or f.endswith(".pyc") or (f.split(".")[len(f.split("."))-1] in include_exts):
-                print "    %s"%os.path.join(dirpath,f)
-                zfile.write(os.path.relpath(os.path.join(dirpath,f)))
+            path = os.path.relpath(os.path.join(dirpath,f))
+            # Package if include source or compile source, and is source file
+            #   or if is pyc file and not compile source
+            #   or if the extension is in include_exts
+            if ((INCLUDE_PROJECT_SOURCE or COMPILE_PROJECT_SOURCE) and f.endswith(".py")) or (f.endswith(".pyc") and not COMPILE_PROJECT_SOURCE) or (f.split(".")[len(f.split("."))-1] in include_exts):
+                print "    %s"%os.path.join(dirpath,f),
+                bytes = open(path,'r').read()
+                
+                # Write Source file if it exists
+                if INCLUDE_PROJECT_SOURCE and path.endswith('.py'):
+                    zfile.writestr(path,bytes)
+
+                # Write Compiled source
+                if COMPILE_PROJECT_SOURCE and path.endswith('.py'):
+                    print " & " + path + 'c',
+                    compiler.compileFile(path)
+                    bytes = open(path+'c','r').read() # Open the generated pyc file
+                    path += 'c'
+                zfile.writestr(path,bytes)
+                print "\n",
 
     # Package Py2D
     print "Packaging Py2D"
