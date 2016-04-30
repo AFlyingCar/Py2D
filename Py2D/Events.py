37b44179e3f149d0d026565713366f82987821ed
Tyler Robbins <tyler@familyrobbins.com>
2016-04-30 13:17:17 -0700

    Added comments. Added a bunch of new Event types that are used with ScreenManager.

--- Py2D/Events.py
+++ Py2D/Events.py
@@ -2,6 +2,10 @@
 from pygame.event import Event
 from pygame.locals import USEREVENT
 
+##################
+# Generic Events #
+##################
+
 # No members
 SCREEN_UPDATE_EVENT = USEREVENT+1
 # 1 member: sound of type Sound
@@ -10,4 +14,24 @@ PLAY_SOUND_EVENT = USEREVENT+2
 SND_FILE_LOADED_EVENT = USEREVENT+3
 # 1 member: thread of type KillableThread
 THREAD_START_EVENT = USEREVENT+4
+# 1 member: current tick number
+TICK_EVENT = USEREVENT+5
+
+#########################
+# Screen Manager Events #
+#########################
+
+# 1 member: renderable of type Image
+SCREEN_MANAGER_ADD_TO_QUEUE_EVENT = USEREVENT+6
+# 2 members: renderable of type Image, oldIndex of type int
+SCREEN_MANAGER_RENDERABLE_REMOVED_FROM_QUEUE_EVENT = USEREVENT+7
+# No members
+PRE_SCREEN_UPDATE_EVENT = USEREVENT+8
+# No members
+RENDER_QUEUE_EVENT = USEREVENT+9
+# No members
+POST_SCREEN_UPDATE_EVENT = USEREVENT+10
+
+# A special base event. Should never be thrown. 255 possible events are allowed after USEREVENT
+CUSTOM_EVENT = USEREVENT+255
 
