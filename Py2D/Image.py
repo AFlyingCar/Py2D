5736f3e46bcbf2a5a1d770fdbb958f417d78842d
Tyler Robbins <tyler@familyrobbins.com>
2016-05-02 19:59:29 -0700

    Added comments. Rearranged methods.

--- Py2D/Image.py
+++ Py2D/Image.py
@@ -17,15 +17,13 @@ class Image(object):
             self.size = [0,0]
         self.angle = 0.0
         self.do_render = True
+
+    ###########
+    # Getters #
+    ###########
     def getDoRender(self):
         return self.do_render
-    def setDoRender(self,value):
-        self.do_render = value
-    def render(self,surf):
-        if self.do_render:
-            s = t.scale(self.surf,self.size)
-            s = t.rotate(s,self.angle)
-            surf.blit(s,self.pos)
+
     def getPosition(self):
         return self.pos
     def getPosX(self):
@@ -33,6 +31,21 @@ class Image(object):
     def getPosY(self):
         return self.pos[1]
 
+    def getSize(self):
+        return self.size
+    def getAngle(self):
+        return self.angle
+    def getSurf(self):
+        return self.surf
+
+    def getFilename(self):
+        return self.filename
+
+    ###########
+    # Setters #
+    ###########
+    def setDoRender(self,value):
+        self.do_render = value
     def setPos(self,pos):
         self.pos = pos
     def setPosX(self,x):
@@ -40,21 +53,33 @@ class Image(object):
     def setPosY(self,y):
         self.pos[1] = y
 
-    def getSize(self):
-        return self.size
-    def getAngle(self):
-        return self.angle
+    def setFilename(self,filename,reLoad=True):
+        self.filename = filename
+        if reLoad:
+            self.loadImage()
+
+    #################
+    # Functionality #
+    #################
+    def render(self,surf):
+        if self.do_render:
+            s = t.scale(self.surf,self.size)
+            s = t.rotate(s,self.angle)
+            surf.blit(s,self.pos)
 
     def scale(self,size):
         self.size = size
     def resetScale(self):
         self.size = self.surf.get_size()
+
     def rotate(self,angle):
         self.angle = angle
     def resetRotation(self):
         self.angle=0.0
+
     def blit(self,surf,pos):
         self.surf.blit(surf,pos)
+
     def loadImage(self):
         if os.path.exists(self.filename):
             self.surf = pygame.image.load(self.filename)
@@ -63,18 +88,10 @@ class Image(object):
             # Create a blank purple surface if the image is not found
             self.surf = pygame.Surface((10,10))
             self.surf.fill((255,0,255))
-    def getFilename(self):
-        return self.filename
-    def setFilename(self,filename,reLoad=True):
-        self.filename = filename
-        if reLoad:
-            self.loadImage()
+
     def save(self,filename=""):
         fname = (filename if filename != "" else self.filename)
         s = t.scale(self.surf,self.size)
         s = t.rotate(s,self.angle)
         pygame.image.save(s,filename)
-
-    def getSurf(self):
-        return self.surf
 
