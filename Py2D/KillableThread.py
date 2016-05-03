db846d2743c411fc44675d8d919de6f604633296
Tyler Robbins <tyler@familyrobbins.com>
2016-05-02 20:00:39 -0700

    Fixed whitespace and line endings.

--- Py2D/KillableThread.py
+++ Py2D/KillableThread.py
@@ -1,31 +1,32 @@
 import sys,trace,threading
 
 class KillableThread(threading.Thread):
-	def __init__(self,*args,**kwargs):
-		threading.Thread.__init__(self,*args,**kwargs)
-		self.killed = False
+    def __init__(self,*args,**kwargs):
+        threading.Thread.__init__(self,*args,**kwargs)
+        self.killed = False
 
-	def start(self):
-		self.__run_backup = self.run
-		self.run = self.__run
-		threading.Thread.start(self)
+    def start(self):
+        self.__run_backup = self.run
+        self.run = self.__run
+        threading.Thread.start(self)
 
-	def __run(self):
-		sys.settrace(self.globaltrace)
-		self.__run_backup()
-		self.run = self.__run_backup
+    def __run(self):
+        sys.settrace(self.globaltrace)
+        self.__run_backup()
+        self.run = self.__run_backup
 
-	def localtrace(self,frame,why,arg):
-		if self.killed:
-			if why == 'line':
-				raise SystemExit()
-		return self.localtrace
+    def localtrace(self,frame,why,arg):
+        if self.killed:
+                if why == 'line':
+                        raise SystemExit()
+        return self.localtrace
 
-	def globaltrace(self,frame,why,arg):
-		if why == 'call':
-			return self.localtrace
-		else:
-			return None
+    def globaltrace(self,frame,why,arg):
+        if why == 'call':
+            return self.localtrace
+        else:
+            return None
 
-	def kill(self):
-		self.killed = True
+    def kill(self):
+        self.killed = True
+
