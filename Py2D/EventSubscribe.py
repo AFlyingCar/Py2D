87ef082e5245d8f0dc52df0a8458e1f9a98a87c3
Tyler Robbins <tyler@familyrobbins.com>
2016-05-16 17:23:30 -0700

    Added a name member to make debugging easier, and so that we can see what the original method's name was.

--- Py2D/EventSubscribe.py
+++ Py2D/EventSubscribe.py
@@ -4,15 +4,19 @@ class EventSubscribe(object):
     def __init__(self,event_type):
         self.event_type = event_type
         self.func = None
+        self.name = ""
         # Create a new list if event_type has not been registered before
         if(event_type not in EventSubscribe.subscribes):
             EventSubscribe.subscribes[event_type] = []
         EventSubscribe.subscribes[event_type].append(self)
     def __call__(self,f):
+        self.name = f.__name__ # Use this to more easily identify which function we're subscribing to
         def wrapped(*args):
             return f(*args)
         self.func = wrapped
         return wrapped
+    def __str__(self):
+        return self.name + ": " + str(self.func)
 
 # @EventSubscribe(/*Event Type*/)
 # def onEvent(event):
