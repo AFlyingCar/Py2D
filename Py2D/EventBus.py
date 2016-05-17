a918209ac41bb05bd440e08e232c0c1502ea70df
Tyler Robbins <tyler@familyrobbins.com>
2016-05-16 17:21:48 -0700

    Rewrote the EventBus class to work with a new eventsystem that uses classes, rather than the pygame.event.Event class with USEREVENT and id numbers. Added a deprecation warning to post(...) about the old ID system.

--- Py2D/EventBus.py
+++ Py2D/EventBus.py
@@ -1,15 +1,53 @@
 import pygame
 from EventSubscribe import EventSubscribe
+from Events.EventBase import EventBase
 
 class EventBus(object):
+    def __init__(self):
+        self.cevents = [] 
+
     def Update(self):
-        for event in pygame.event.get():
-            for method in EventSubscribe.subscribes[event.type]:
+        # Get the pygame events
+        # Add it so that we APPEND the two lists, not overwrite it
+        self.cevents += pygame.event.get()
+
+        for event in self.cevents:
+            if(type(event) == pygame.event.Event):
+                eid = event.type
+            else:
+                # Use the type of event.
+                # We assume here that the event extends EventBase
+                eid = type(event)
+
+            # If the event isn't requested by anything, then skip it
+            # We do this because if an unhandled event is posted, then a KeyError is thrown
+            if(eid not in EventSubscribe.subscribes):
+                continue
+
+            for method in EventSubscribe.subscribes[eid]:
                 method.func(event)
 
+            if(type(event) == EventBase):
+                event.lifecycle+=1
+                if(event.lifecycle > event.MAX_LIFECYCLE):
+                    event.canceled = True
+
+                if(event.canceled):
+                    self.cevents.remove(event)
+            else:
+                self.cevents.remove(event)
+            # TODO: Do we need an else here? What if they don't put in an EventBase
+
     def post(self,eid,**kwargs):
-        event = pygame.event.Event(eid,**kwargs)
-        pygame.event.post(event)
+        # This weird type-check is here to ensure backwards compatability
+        if(type(eid) == int):
+            print "Posting old pygame-style event."
+            print "Please note that this way is deprecated. Use the new class way instead."
+            event = pygame.event.Event(eid,**kwargs)
+            pygame.event.post(event)
+        else:
+            # Hopefully use our own event system
+            self.cevents.append(eid)
 
 EVENT_BUS = EventBus()
 
