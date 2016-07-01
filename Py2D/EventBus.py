3f07cf774860939a766c5495e04cd0f7036fa961
Tyler Robbins <tyler@familyrobbins.com>
2016-07-01 15:35:28 -0700

    Added support for the pygame event wrapper classes. Changed how pygame events are inserted into our own event system.

--- Py2D/EventBus.py
+++ Py2D/EventBus.py
@@ -1,6 +1,24 @@
 import pygame
+from pygame.locals import *
 from EventSubscribe import EventSubscribe
 from Events.EventBase import EventBase
+#from Events.Pygame import *
+# Import every pygame event
+from Events.Pygame.QuitEvent import QuitEvent 
+from Events.Pygame.ActiveEvent import ActiveEvent 
+from Events.Pygame.KeyDownEvent import KeyDownEvent 
+from Events.Pygame.KeyUpEvent import KeyUpEvent 
+from Events.Pygame.MouseMotionEvent import MouseMotionEvent 
+from Events.Pygame.MouseButtonUpEvent import MouseButtonUpEvent 
+from Events.Pygame.MouseButtonDownEvent import MouseButtonDownEvent 
+from Events.Pygame.JoyAxisMotionEvent import JoyAxisMotionEvent 
+from Events.Pygame.JoyBallMotionEvent import JoyBallMotionEvent 
+from Events.Pygame.JoyHatMotionEvent import JoyHatMotionEvent 
+from Events.Pygame.JoyButtonUpEvent import JoyButtonUpEvent 
+from Events.Pygame.JoyButtonDownEvent import JoyButtonDownEvent 
+from Events.Pygame.VideoResizeEvent import VideoResizeEvent 
+from Events.Pygame.VideoExposeEvent import VideoExposeEvent 
+from Events.Pygame.PygameUserEvent import PygameUserEvent 
 
 class EventBus(object):
     def __init__(self):
@@ -8,8 +26,15 @@ class EventBus(object):
 
     def Update(self):
         # Get the pygame events
+        pygame_events = pygame.event.get()
+        # I would optimize this, but the truth of the matter is that there are rarely more than 2 or 3 pygame events in the queue at once
+        # If I think it is really bad, then I'll probably do something to check if there are even any events in the queue.
+        # But for now it is on the bottom of my priority list
+        for event in pygame_events:
+            self.cevents.append(self.convertPygameEventToClassEvent(event))
+
         # Add it so that we APPEND the two lists, not overwrite it
-        self.cevents += pygame.event.get()
+        #self.cevents += pygame_events
 
         for event in self.cevents:
             if(type(event) == pygame.event.Event):
@@ -48,6 +73,40 @@ class EventBus(object):
         else:
             # Hopefully use our own event system
             self.cevents.append(eid)
+
+    def convertPygameEventToClassEvent(self,event):
+        if(event.type == QUIT):
+            return QuitEvent(event)
+        elif(event.type == ACTIVEEVENT):
+            return ActiveEvent(event)
+        elif(event.type == KEYDOWN):
+            return KeyDownEvent(event)
+        elif(event.type == KEYUP):
+            return KeyUpEvent(event)
+        elif(event.type == MOUSEMOTION):
+            return MouseMotionEvent(event)
+        elif(event.type == MOUSEBUTTONUP):
+            return MouseButtonUpEvent(event)
+        elif(event.type == MOUSEBUTTONDOWN):
+            return MouseButtonDownEvent(event)
+        elif(event.type == JOYAXISMOTION):
+            return JoyAxisMotionEvent(event)
+        elif(event.type == JOYBALLMOTION):
+            return JoyBallMotionEvent(event)
+        elif(event.type == JOYHATMOTION):
+            return JoyHatMotionEvent(event)
+        elif(event.type == JOYBUTTONUP):
+            return JoyButtonUpEvent(event)
+        elif(event.type == JOYBUTTONDOWN):
+            return JoyButtonDownEvent(event)
+        elif(event.type == VIDEORESIZE):
+            return VideoResizeEvent(event)
+        elif(event.type == VIDEOEXPOSE):
+            return VideoExposeEvent(event)
+        elif(event.type == USEREVENT):
+            return PygameUserEvent(event)
+        else:
+            return event.type
 
 EVENT_BUS = EventBus()
 
